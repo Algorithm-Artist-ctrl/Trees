4da337223a0919772cba528c70b282e89ccf8f5f
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-03 11:10:00 +0530

    Height of Tree

---
+++ Height_of_tree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def height(root):
+    if root is None:
+        return 0
+    return 1 + max(height(root.left), height(root.right))
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+
+print(height(root))  
