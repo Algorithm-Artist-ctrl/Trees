ab104b2f8b29351d1602b77a48c7703b1e6ec79c
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-05 17:52:28 +0530

    Balance Tree

---
+++ balance_tree.py
@@ -0,0 +1,40 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        def height(node):
+            if not node:
+                return 0
+
+            left_height = height(node.left)
+            if left_height == -1:
+                return -1
+
+            right_height = height(node.right)
+            if right_height == -1:
+                return -1
+
+            if abs(left_height - right_height) > 1:
+                return -1
+
+            return 1 + max(left_height, right_height)
+
+        return height(root) != -1
+
+if __name__ == "__main__":
+    root1 = TreeNode(1)
+    root1.left = TreeNode(2)
+    root1.right = TreeNode(3)
+    root1.left.left = TreeNode(4)
+    root1.left.right = TreeNode(5)
+
+    root2 = TreeNode(1)
+    root2.right = TreeNode(2)
+    root2.right.right = TreeNode(3)
+
+    sol = Solution()
+    print(sol.isBalanced(root1)) 
+    print(sol.isBalanced(root2))  #
