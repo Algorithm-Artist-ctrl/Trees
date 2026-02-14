596eed28fd8ba7ef5e865ba91decba714cfd961b
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-14 18:22:15 +0530

    Largest value in Tree

--- largest_value.py
+++ largest_value.py
@@ -1,15 +1,12 @@
-
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-
 class Solution:
     def largestValues(self, root: TreeNode):
         if not root:
             return []
-
         result = []
         queue = [root]  
 
