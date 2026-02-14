a43f6027422a660233014a5575cf721689aa6a02
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-14 18:25:07 +0530

    Sum Node

--- Sum_of_node.py
+++ Sum_of_node.py
@@ -3,7 +3,6 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
-
 def sum_of_nodes(root):
     if root is None:
         return 0
@@ -12,5 +11,4 @@ root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
-
 print(sum_of_nodes(root)) 
