a6dd6d7fdc1207f0dfb131e483fc9bd6e745319c
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-14 18:24:27 +0530

    Define Tree

--- defining.py
+++ defining.py
@@ -2,14 +2,11 @@ class TreeNode:
     def __init__(self,data):
         self.data=data
         self.childern=[]
-
 root = TreeNode(1)
 child1=TreeNode(2)
 child2=TreeNode(3)
 child3=TreeNode(4)
-
 root.childern.append(child1)
 root.childern.append(child2)
 root.childern.append(child3)
-
 print(root.childern[0].data)
