12f10fa1a253096048b6ce1b376ab35955f4b428
Tarun Kushwah <tarunkushwahkt@gmail.com>
2026-02-15 13:15:16 +0530

    Height of Tree

--- Height_of_tree.py
+++ Height_of_tree.py
@@ -3,7 +3,6 @@ class Node:
         self.value = value
         self.left = None
         self.right = None
-
 def height(root):
     if root is None:
         return 0
@@ -12,5 +11,4 @@ root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
-
 print(height(root))  
